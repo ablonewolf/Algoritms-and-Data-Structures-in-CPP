512075fd131e095bf048cf28f696ed22240ab81b
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-03-29 05:00:14 +0600

    code for factorial of a number in recursive approach.

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int calculateFactorial(int number)
+{
+  if (number == 0)
+    return 1;
+  return number * calculateFactorial(number - 1);
+}
+
+int main()
+{
+  cout << "Enter the number to find its factorial." << endl;
+  cout << "Number: ";
+  int number;
+  cin >> number;
+  cout << "The factorial of " << number << " is : " << calculateFactorial(number) << endl;
+}
