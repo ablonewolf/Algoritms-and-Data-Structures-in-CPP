3217230cbbf31bd538da40232f6e8a15c46e06d2
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-04-02 12:43:47 +0600

    Implemented combination formula in a recursive way.

---
+++ Recursion/CombinationFormulaUsingRecursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int findNumberOfCombinations(int n, int r)
+{
+  if (r == 0 || n == r)
+  {
+    return 1;
+  }
+  if (r == 1)
+  {
+    return n;
+  }
+  return findNumberOfCombinations(n - 1, r - 1) + findNumberOfCombinations(n - 1, r);
+}
+
+int main()
+{
+  cout << "Enter the value of n and r to find the number of combinations." << endl;
+  int n, r;
+  cout << "n :";
+  cin >> n;
+  cout << "r :";
+  cin >> r;
+  cout << "The number of combinations for " << n << " and " << r << " is: " << findNumberOfCombinations(n, r) << endl;
+}
