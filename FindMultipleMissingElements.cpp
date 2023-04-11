a743b7aba610244fa429c6d201ab495f095d70e7
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-04-12 00:44:26 +0600

    code to find multiple missing elements in an array.

---
+++ FindMultipleMissingElements.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+void countMissingElements(int *array, int size, int &count)
+{
+  int i = 0;
+  int initialDiff = array[i] - i;
+  while (i < size)
+  {
+    if (initialDiff != array[i] - i)
+    {
+      while (initialDiff < array[i] - i)
+      {
+        count++;
+        initialDiff++;
+      }
+    }
+    i++;
+  }
+}
+
+void printMissingElements(int *array, int size)
+{
+  cout << "The missing elements in the array are printed below." << endl;
+  int initialDiff = array[0] - 0;
+  for (int i = 0; i < size; i++)
+  {
+    if (initialDiff != array[i] - i)
+    {
+      while (initialDiff < array[i] - i)
+      {
+        if (initialDiff == array[0] - 0)
+        {
+          cout << i + initialDiff;
+        }
+        else
+        {
+          cout << " " << i + initialDiff;
+        }
+        initialDiff++;
+      }
+    }
+  }
+  cout << endl;
+}
+
+bool isSequentaillySorted(int *array, int size)
+{
+  bool sequentiallySorted = true;
+  for (int i = 0; i < size - 1; i++)
+  {
+    if (array[i] < array[i + 1])
+    {
+      continue;
+    }
+    else
+    {
+      sequentiallySorted = false;
+      break;
+    }
+  }
+  return sequentiallySorted;
+}
+
+int main()
+{
+  cout << "Enter the size of the array." << endl;
+  cout << "Size: ";
+  int size, count = 0;
+  cin >> size;
+  cout << "Enter the elements of the array in ascending order." << endl;
+  int *array = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    cout << "Array[" << i + 1 << "]: ";
+    cin >> array[i];
+  }
+  if (isSequentaillySorted(array, size))
+  {
+    countMissingElements(array, size, count);
+    if (count > 0)
+    {
+      cout << count << " elements are missing in the array." << endl;
+      printMissingElements(array, size);
+    }
+    else
+    {
+      cout << "No missing elements in the array." << endl;
+    }
+  }
+  else
+  {
+    cout << "The array is not sequentially sorted." << endl;
+  }
+}
