3a51313357e8cb848771bfb86663717b542ad22c
Arka Bhuiyan <arkabhuiyancsedu@gmail.com>
2023-04-17 04:39:29 +0600

    code to find the length of the string.

---
+++ Strings/CalculateLength.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int findLength(string str)
+{
+  int length = 0;
+  for (int i = 0; str[i]; i++)
+  {
+    length++;
+  }
+  return length;
+}
+
+int main()
+{
+  string str;
+  cout << "Please enter the string : ";
+  getline(cin, str);
+  cout << "The length of the string is : " << findLength(str) << endl;
+}
