cf88812fd8be472d8807cd32ec3d9ff8bbd1b2aa
Arka Bhuiyan <arkabhuiyancsedu@gmail.com>
2023-04-17 04:47:21 +0600

    code for changing case of letter in a string.

---
+++ Strings/ChangeCaseOfLetter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+string changeCase(string str)
+{
+    for (int i = 0; str[i]; i++)
+    {
+        if (str[i] >= 97 && str[i] <= 122)
+        {
+            str[i] = str[i] - 32;
+        }
+        else if (str[i] >= 65 && str[i] <= 90)
+        {
+            str[i] = str[i] + 32;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return str;
+}
+
+int main()
+{
+    string str;
+    cout << "Please enter the string : ";
+    getline(cin, str);
+    cout << "After changing the case, the string is: " << changeCase(str) << endl;
+}
