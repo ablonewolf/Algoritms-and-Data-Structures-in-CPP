c88977bf0bebb0c683d4aee6cf7dd30558fa89cb
Arka Bhuiyan <arkabhuiyancsedu@gmail.com>
2023-04-17 06:02:56 +0600

    code for reversing a string in optimized mode.

---
+++ Strings/ReverseString.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void swapCharacters(char &a, char &b)
+{
+    char temp = a;
+    a = b;
+    b = temp;
+}
+
+string reverseString(string str)
+{
+    int j;
+    for (int i = 0; str[i]; i++)
+    {
+        j++;
+    }
+    j--;
+    int i = 0;
+    while (i < j)
+    {
+        swapCharacters(str[i], str[j]);
+        i++;
+        j--;
+    }
+    return str;
+}
+
+int main()
+{
+    cout << "Enter the string to reverse." << endl;
+    cout << "The String: ";
+    string str;
+    cin >> str;
+    cout << "After reversing, the string is: " << reverseString(str) << "." << endl;
+}
