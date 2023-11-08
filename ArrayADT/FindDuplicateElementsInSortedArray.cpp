015ce0ee8577db59c6492e22b19ae33faa39a7e1
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-11-08 07:04:56 +0600

    reformatted two files.

--- ArrayADT/FindDuplicateElementsInSortedArray.cpp
+++ ArrayADT/FindDuplicateElementsInSortedArray.cpp
@@ -1,75 +1,79 @@
 #include <iostream>
+
 using namespace std;
 
-int printDuplicateElements(int *array, int size)
+int printDuplicateElement(int *array, int size)
 {
-  int j, count = 0;
-  for (int i = 0; i < size; i++)
-  {
-    if (array[i] == array[i + 1])
+    int j, count = 0;
+    for (int i = 0; i < size; i++)
     {
-      count++;
-      j = i + 1;
-      while (array[i] == array[j])
-      {
-        j++;
-      }
-      cout << array[i] << " is appearing " << j - i << " times." << endl;
-      i = j - 1;
+        if (array[i] == array[i + 1])
+        {
+            count++;
+            j = i + 1;
+            while (array[i] == array[j])
+            {
+                j++;
+            }
+            cout << array[i] << " is appearing " << j - i << " times." << endl;
+            i = j - 1;
+        }
+        else
+        {
+            continue;
+        }
     }
-    else
-    {
-      continue;
-    }
-  }
-  return count;
+    return count;
 }
-bool isSequentaillySorted(int *array, int size)
+
+bool isSequentiallySorted(int *array, int size)
 {
-  bool sequentiallySorted = true;
-  for (int i = 0; i < size - 1; i++)
-  {
-    if (array[i] < array[i + 1] && array[i] > 0)
+    bool sequentiallySorted = true;
+    for (int i = 0; i < size - 1; i++)
     {
-      continue;
+        if (array[i] < array[i + 1] && array[i] > 0)
+        {
+            continue;
+        }
+        else
+        {
+            sequentiallySorted = false;
+            break;
+        }
     }
-    else
-    {
-      sequentiallySorted = false;
-      break;
-    }
-  }
-  return sequentiallySorted;
+    return sequentiallySorted;
 }
 
 int main()
 {
-  cout << "Enter the size of the array." << endl;
-  cout << "Size: ";
-  int size;
-  cin >> size;
-  int *array = new int[size];
-  cout << "Enter the elements of the array in ascending order." << endl;
-  for (int i = 0; i < size; i++)
-  {
-    cout << "Array[" << i + 1 << "]: ";
-    cin >> array[i];
-  }
-  if (isSequentaillySorted(array, size))
-  {
-    int count = printDuplicateElements(array, size);
-    if (count == 0)
+    cout << "Enter the size of the array." << endl;
+    cout << "Size: ";
+    int size;
+    cin >> size;
+    int *array = new int[size];
+    cout << "Enter the elements of the array in ascending order." << endl;
+    for (int i = 0; i < size; i++)
     {
-      cout << "No duplicates found in this array" << endl;
+        cout << "Array[" << i + 1 << "]: ";
+        cin >> array[i];
+    }
+    if (isSequentiallySorted(array, size))
+    {
+        int count = printDuplicateElement(array, size);
+        if (count == 0)
+        {
+            cout << "No duplicates found in this array" << endl;
+        }
+        else
+        {
+            cout << "Number of duplicates in this array is: " << count << "." << endl;
+        }
     }
     else
     {
-      cout << "Number of duplicates in this array is: " << count << "." << endl;
+        cout
+            << "The array is not sorted in asceding order or some elements of it are not greater than zero. So terminating operation."
+            << endl;
     }
-  }
-  else
-  {
-    cout << "The array is not sorted in asceding order or some elements of it are not greater than zero. So terminating operation." << endl;
-  }
-  delete array;
+    delete array;
 }
--- ArrayADT/FindMultipleMissingElements.cpp
+++ ArrayADT/FindMultipleMissingElements.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 
 // function for counting missing elements.
-void countMissingElements(int *array, int size, int &count) {
+void countMissingElements(int *array, int size, int &count)
+{
     int i = 0;
     int initialDiff = array[i] - i;
-    while (i < size) {
-        if (initialDiff != array[i] - i) {
-            while (initialDiff < array[i] - i) {
+    while (i < size)
+    {
+        if (initialDiff != array[i] - i)
+        {
+            while (initialDiff < array[i] - i)
+            {
                 count++;
                 initialDiff++;
             }
@@ -17,15 +21,22 @@ void countMissingElements(int *array, int size, int &count) {
     }
 }
 
-void printMissingElements(int *array, int size) {
+void printMissingElements(int *array, int size)
+{
     cout << "The missing elements in the array are printed below." << endl;
     int initialDiff = array[0] - 0; // set up the initial difference value
-    for (int i = 0; i < size; i++) {
-        if (initialDiff != array[i] - i) {
-            while (initialDiff < array[i] - i) {
-                if (initialDiff == array[0] - 0) { // check if this is the fist missing element
+    for (int i = 0; i < size; i++)
+    {
+        if (initialDiff != array[i] - i)
+        {
+            while (initialDiff < array[i] - i)
+            {
+                if (initialDiff == array[0] - 0)
+                { // check if this is the fist missing element
                     cout << i + initialDiff;
-                } else {
+                }
+                else
+                {
                     cout << " " << i + initialDiff;
                 }
                 initialDiff++;
@@ -36,12 +47,17 @@ void printMissingElements(int *array, int size) {
 }
 
 // function to check whether the array is sorted or not.
-bool isSequentiallySorted(int *array, int size) {
+bool isSequentiallySorted(int *array, int size)
+{
     bool sequentiallySorted = true;
-    for (int i = 0; i < size - 1; i++) {
-        if (array[i] < array[i + 1]) {
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (array[i] < array[i + 1])
+        {
             continue;
-        } else {
+        }
+        else
+        {
             sequentiallySorted = false;
             break;
         }
@@ -49,26 +65,34 @@ bool isSequentiallySorted(int *array, int size) {
     return sequentiallySorted;
 }
 
-int main() {
+int main()
+{
     cout << "Enter the size of the array." << endl;
     cout << "Size: ";
     int size, count = 0;
     cin >> size;
     cout << "Enter the elements of the array in ascending order." << endl;
     int *array = new int[size];
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << "Array[" << i + 1 << "]: ";
         cin >> array[i];
     }
-    if (isSequentiallySorted(array, size)) {
+    if (isSequentiallySorted(array, size))
+    {
         countMissingElements(array, size, count);
-        if (count > 0) {
+        if (count > 0)
+        {
             cout << count << " elements are missing in the array." << endl;
             printMissingElements(array, size);
-        } else {
+        }
+        else
+        {
             cout << "No missing elements in the array." << endl;
         }
-    } else {
+    }
+    else
+    {
         cout << "The array is not sequentially sorted." << endl;
     }
 }
