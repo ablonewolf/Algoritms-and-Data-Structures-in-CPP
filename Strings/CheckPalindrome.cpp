d601c96f6973b7511bb6c71387b12dac8a099f00
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-12-18 02:39:48 +0600

    Added case check while checking palindrome.

--- Strings/CheckPalindrome.cpp
+++ Strings/CheckPalindrome.cpp
@@ -11,14 +11,29 @@ int findLength(string str)
   return length;
 }
 
+string makeLowerCase(string word)
+{
+  int index = 0;
+  while (word[index] != '\0')
+  {
+    if (word[index] >= 65 && word[index] <= 90)
+    {
+      word[index] = word[index] + 32;
+    }
+    index++;
+  }
+  return word;
+}
+
 bool checkPalindrome(string word)
 {
+  string convertedLowerCaseWord = makeLowerCase(word);
   bool isPalindrome = true;
-  int lastIndex = findLength(word) - 1;
+  int lastIndex = findLength(convertedLowerCaseWord) - 1;
   int initialIndex = 0;
   while (initialIndex < lastIndex)
   {
-    if (word[initialIndex] == word[lastIndex])
+    if (convertedLowerCaseWord[initialIndex] == convertedLowerCaseWord[lastIndex])
     {
       initialIndex++;
       lastIndex--;
@@ -34,7 +49,7 @@ bool checkPalindrome(string word)
 
 int main()
 {
-  cout << "Enter the word to check whether it's palindrome or not. Please enter all characters in lower case. " << endl;
+  cout << "Enter the word to check whether it's palindrome or not. Please enter all characters in lower case." << endl;
   cout << "The Word: ";
   string word;
   cin >> word;
