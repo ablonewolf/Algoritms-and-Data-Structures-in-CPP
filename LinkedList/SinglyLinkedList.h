e791f00203bffedd8fd1e1d6a5715e99c0b5e95f
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-01-05 12:45:24 +0600

    Refactored the code for class as a separate class instead of inner class.

--- LinkedList/SinglyLinkedList.h
+++ LinkedList/SinglyLinkedList.h
@@ -2,25 +2,25 @@
 #define default_value -9999999
 using namespace std;
 
+class Node
+{
+public:
+  int value;
+  Node *next = nullptr;
+  Node(int value)
+  {
+    this->value = value;
+    this->next = nullptr;
+  }
+  Node()
+  {
+    this->value = default_value;
+    this->next = nullptr;
+  }
+};
 class SinglyLinkedList
 {
 public:
-  class Node
-  {
-  public:
-    int value;
-    Node *next = nullptr;
-    Node(int value)
-    {
-      this->value = value;
-      this->next = nullptr;
-    }
-    Node()
-    {
-      this->value = default_value;
-      this->next = nullptr;
-    }
-  };
   SinglyLinkedList()
   {
     this->head = nullptr;
