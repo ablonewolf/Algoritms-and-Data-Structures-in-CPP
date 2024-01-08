36d0823160281e639e5cbca11e6c3cb786a44724
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-01-09 05:47:31 +0600

    Added class for stack containing all its operations, along with a driver program

---
+++ Stack/Stack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+int main()
+{
+  Stack *stack = new Stack();
+  while (1)
+  {
+    cout << "Enter 1 to push new values to the stack" << endl;
+    cout << "Enter 2 to print the top of the stack." << endl;
+    cout << "Enter 3 to perform the pop operation." << endl;
+    cout << "Enter 4 to print all values stored inside the stack." << endl;
+    cout << "Enter 5 to exit the application." << endl;
+    int option;
+    cout << "Enter option: ";
+    cin >> option;
+    if (option == 1)
+    {
+      cout << "Enter the value to push into the stack: ";
+      int value;
+      cin >> value;
+      stack->push(value);
+    }
+    else if (option == 2)
+    {
+      cout << "The current top of the stack is " << stack->top() << "." << endl;
+    }
+    else if (option == 3)
+    {
+      cout << "The popped item from the stack is " << stack->pop() << "." << endl;
+      cout << "After popping, the current top of the stack is " << stack->top() << "." << endl;
+    }
+    else if (option == 4)
+    {
+      stack->printStack();
+    }
+    else if (option == 5)
+    {
+      cout << "Thank you for using this application." << endl;
+      break;
+    }
+    else
+    {
+      cout << "Invalid option. Please try again." << endl;
+    }
+  }
+  delete stack;
+}
---
+++ Stack/Stack.h
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class definition for node
+class Node
+{
+public:
+  int value;
+  Node *next;
+
+  // default constructor with no value passed,
+  // in this case, the value will be null
+
+  // default constructor when value is passed
+  Node(int value)
+  {
+    this->value = value;
+    this->next = nullptr;
+  }
+};
+
+// class definition for stack
+class Stack
+{
+public:
+  Node *head;
+
+  // default constructor with no value
+  //  in this case, head will be null
+  Stack()
+  {
+    this->head = nullptr;
+  }
+
+  // default constructor with value passed
+  Stack(int value)
+  {
+    this->head->value = value;
+  }
+
+  // method for pushing value into the stack
+  // whenever we push a value, it will be saved as the first element; often we call it the top
+  void push(int value)
+  {
+    Node *node = new Node(value);
+    if (this->head == nullptr)
+    {
+      this->head = node;
+    }
+    else
+    {
+      Node *tempNode = this->head;
+      node->next = tempNode;
+      this->head = node;
+    }
+  }
+
+  // method for returning the top-most value
+  int top()
+  {
+    return this->head->value;
+  }
+
+  // method for removing the current-top and returning its value
+  int pop()
+  {
+    int topValue = this->head->value;
+    this->head = this->head->next;
+    return topValue;
+  }
+
+  bool isEmpty()
+  {
+    return this->head == nullptr;
+  }
+
+  void printStack()
+  {
+    if (this->head == nullptr)
+    {
+      cout << "The stack is currently empty." << endl;
+    }
+    else
+    {
+      cout << "The present items in the stack are listed below." << endl;
+      Node *tempNode = this->head;
+      while (tempNode != nullptr)
+      {
+        if (tempNode == this->head)
+        {
+          cout << tempNode->value;
+        }
+        else
+        {
+          cout << "->" << tempNode->value;
+        }
+        tempNode = tempNode->next;
+      }
+      cout << endl;
+    }
+  }
+};
