548f8d13d8ed2b411005695ef903414011ad4b63
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-01-09 07:07:55 +0600

    added balance check for other brackets and commented the whole logic.

--- Stack/ParenthesisBalanceCheck.cpp
+++ Stack/ParenthesisBalanceCheck.cpp
@@ -2,51 +2,47 @@
 #include "CharacterStack.h"
 using namespace std;
 
-bool isValidEquation(string equation)
-{
-  int index = equation.find('(');
-  if (index >= 0)
-  {
-    return true;
-  }
-  return false;
-}
-
 int main()
 {
   string equation;
   cout << "Enter the equation to check whether its parenthesis are balanced: ";
+  // we are using the get line function so that we can take an entire line as input
   getline(cin, equation);
 
-  if (isValidEquation(equation))
+  Stack *parenthesisStack = new Stack();
+  for (int i = 0; i < equation.length(); i++)
   {
-    Stack *parenthesisStack = new Stack();
-    for (int i = 0; i < equation.length(); i++)
+    if (equation[i] == '(' ||
+        equation[i] == '{' ||
+        equation[i] == '[')
     {
-      if (equation[i] == '(')
-      {
-        parenthesisStack->push(equation[i]);
-      }
-      else if (equation[i] == ')')
-      {
+      // we will only store the opening brackets
+      parenthesisStack->push(equation[i]);
+    }
+    // if we encounter a closing bracket
+    else if (equation[i] == ')' || equation[i] == '}' || equation[i] == ']')
+    {
+      // storing the current top of the stack here
+      char top = parenthesisStack->top();
+      // if the closing bracket is the matched for the top, then we will pop that top.
+      if ((equation[i] == ')' && top == '(') ||
+          (equation[i] == '}' && top == '{') ||
+          (equation[i] == ']' && top == '['))
         parenthesisStack->pop();
-      }
-      else
-      {
-        continue;
-      }
     }
-    if (parenthesisStack->isEmpty())
-    {
-      cout << "The equation is balanced." << endl;
-    }
+    // ignore the other characters
     else
     {
-      cout << "The equation is not balanced." << endl;
+      continue;
     }
+  }
+  // the stack should be emtpy if the brackets in the equation are balanced
+  if (parenthesisStack->isEmpty())
+  {
+    cout << "The equation is balanced." << endl;
   }
   else
   {
-    cout << "It's an invalid eqaution." << endl;
+    cout << "The equation is not balanced." << endl;
   }
 }
