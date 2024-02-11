03471ad207e5e19aca707f302aa699e439efa5e7
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-02-11 10:29:01 +0600

    solved the problem of n-k ladder using the bottom-up approach.

---
+++ DynamicProgramming/N-K-Ladders.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int calculateSteps(int n, int k) {
+  vector<int> stepArray(n + 1, 0);
+  // initialize values for base case
+  stepArray[0] = 1;
+  stepArray[1] = 1;
+
+  for (int i = 2; i <= n; i++) {
+    // if the current step is greater than K, we will consider the previous K steps and sum them up
+    if (i >= k) {
+      int temp = k;
+      while (temp > 0) {
+        stepArray[i] += stepArray[i - temp];
+        temp--;
+      }
+      // if not, then we will just increment by 1 from the previous step
+    } else {
+      stepArray[i] = stepArray[i - 1] + 1;
+    }
+  }
+
+  return stepArray[n];
+}
+
+int main() {
+  cout << "Enter the total number of steps: ";
+  int steps;
+  cin >> steps;
+  cout << "Enter the length of at most max step: ";
+  int max;
+  cin >> max;
+  int answer = calculateSteps(steps, max);
+  cout << "The total number of ways to reach that step is " << answer << "." << endl;
+}
+
